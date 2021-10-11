22d723e1a0285dc21f24920a657d1bc976110fb7
reuixiy <reuixiy@gmail.com>
2021-10-11 16:56:46 +0800

    feat: set up react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg'
 import './App.css'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>BestSearch</h1>
       </header>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <p>/</p>
+          </Route>
+          <Route exact path="/search">
+            <p>/search</p>
+          </Route>
+
+          {/* 404 */}
+          <Route path="*">
+            {/* <NoMatch /> */}
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </main>
     </div>
   )
 }
